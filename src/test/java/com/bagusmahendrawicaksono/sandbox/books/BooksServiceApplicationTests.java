a329d019803c606490c7b7485abe75430b485085
Bagus Mahendra Wicaksono <bagusmwicaksono@gmail.com>
2023-07-13 22:40:30 +0800

    Initial Commit

---
+++ src/test/java/com/bagusmahendrawicaksono/sandbox/books/BooksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagusmahendrawicaksono.sandbox.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
